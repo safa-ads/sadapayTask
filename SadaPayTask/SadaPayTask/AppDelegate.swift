123a018f83f3c7ad9b79a9f78a5ef9dde07be3b7
Safa Ads <safa.ads@swvl.com>
2023-06-15 20:11:43 +0300

    initial project setup

---
+++ SadaPayTask/SadaPayTask/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SadaPayTask
+//
+//  Created by Safa Ads on 15/06/2023.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ SadaPayTask/SadaPayTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SadaPayTask
+//
+//  Created by Safa Ads on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
