71784088d06c2f262b53a75cd750a9572fabab90
Safa Ads <safa.ads@swvl.com>
2023-06-16 22:51:25 +0300

    Impelement `getRepositories()` in usecase

--- SadaPayTask/UseCase/GithubRepositoriesUseCase.swift
+++ SadaPayTask/UseCase/GithubRepositoriesUseCase.swift
@@ -6,12 +6,18 @@
 //
 
 protocol GithubRepositoriesUseCaseProtocol {
+    func getRepositories(completion: @escaping (Result<GitHubRepository, Error>) -> Void)
 }
 public class GithubRepositoriesUseCase: GithubRepositoriesUseCaseProtocol {
-    
     public let repo: GithubRepositoriesRepoProtocol
     
     public init(repo: GithubRepositoriesRepoProtocol = GithubRepositoriesRepo()) {
         self.repo = repo
+    }
+    
+    func getRepositories(completion: @escaping (Result<GitHubRepository, Error>) -> Void) {
+        repo.getRepositories { response in
+            completion(response)
+        }
     }
 }
