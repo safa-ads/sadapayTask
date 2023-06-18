e869fe7ac50aa80764c7bf8c6d327faecbb0828f
Safa Ads <safa.ads@swvl.com>
2023-06-19 01:29:33 +0300

    Add `getRepositories` call in view controller

--- SadaPayTask/SadaPayTask/GithubRepositories/GithubRepositoriesViewController.swift
+++ SadaPayTask/SadaPayTask/GithubRepositories/GithubRepositoriesViewController.swift
@@ -22,6 +22,7 @@ class GithubRepositoriesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewModel()
+        viewModel?.getRepositories()
     }
 }
 
